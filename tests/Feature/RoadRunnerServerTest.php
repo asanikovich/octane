5876ee0f612417c5ca55b34cce9f1379eaf0d3ec
Aliaksei Sanikovich <asanikovich@gmail.com>
2023-06-09 10:40:28 +0300

    2

--- tests/Feature/RoadRunnerServerTest.php
+++ tests/Feature/RoadRunnerServerTest.php
@@ -52,15 +52,28 @@ class RoadRunnerServerTest extends TestCase
             '--log-level=debug',
         ]);
 
+        $cmd = implode(' ', [
+            (new PhpExecutableFinder)->find(),
+            $this->path.'/artisan',
+            'octane:start',
+            '--server=roadrunner',
+            '--host=127.0.0.1',
+            '--port=22622',
+            '--workers=1',
+            '--task-workers=1',
+            '--log-level=debug',
+        ]);
+        exec($cmd, $output);
+        dump($output);
+
         $httpClient = new Guzzle(['base_uri' => 'http://127.0.0.1:22622']);
 
         // to preventing child process blocking <https://www.php.net/manual/ru/function.proc-open.php#38870>
-        //$rrProc->disableOutput();
+        $rrProc->disableOutput();
 
         try {
             // https://symfony.com/doc/current/components/process.html#running-processes-asynchronously
             $rrProc->start();
-            dump($rrProc->getOutput());
 
             $this->waitUntilServerIsStarted($httpClient);
             $this->checkServerHttp($httpClient);
